d5ca3efb7a4896062865a8288b2b37dcec20bc9a
dhanashree2200 <dhanashreekp2200@gmail.com>
2021-04-24 18:31:14 +0530

    category page added

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
+
+import 'category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GridView(
-        children: [],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meal App"),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((catDate) => CategoryItem(
+                  title: catDate.title,
+                  color: catDate.color,
+                ))
+            .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
+            maxCrossAxisExtent: 300,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20),
